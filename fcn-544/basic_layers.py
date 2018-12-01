ce110fa13ae5248d0be37548842ab360790b86e8
zliu63 <zliu63@illinois.edu>
2018-11-30 23:16:15 -0600

    ..

--- fcn-544/basic_layers.py
+++ fcn-544/basic_layers.py
@@ -3,24 +3,24 @@ from caffe import layers
 from caffe import params
 
 
-def conv(prev, nout, ks=3, stride=1, pad=1):
-	return layers.Convolution(prev, kernel_size = ks, stride = stride,
+def conv(bottom, nout, ks=3, stride=1, pad=1):
+	return layers.Convolution(bottom, kernel_size = ks, stride = stride,
 								num_output=nout, pad = pad,
 								param = [dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)])
 
 
-def relu(prev):
-	return layers.ReLu(prev, in_place=True)
+def relu(bottom):
+	return layers.ReLu(bottom, in_place=True)
 
-def max_pooling(prev, ks=2, stride=2):
-	return layers.Pooling(prev, pool=params.Pooling.MAX, kernel_size=ks, stride=stride)
+def max_pooling(bottom, ks=2, stride=2):
+	return layers.Pooling(bottom, pool=params.Pooling.MAX, kernel_size=ks, stride=stride)
 
 
-def dropout(prev, ratio = 0.5, in_place = True):
-	return layers.Dropout(prev, dropout_ratio=ratio, in_place = in_place)
+def dropout(bottom, ratio = 0.5, in_place = True):
+	return layers.Dropout(bottom, dropout_ratio=ratio, in_place = in_place)
 
-def deconv(prev, nout, ks=4, stride=2, bias_term=False):
-	return layers.Deconvolution(prev,
+def deconv(bottom, nout, ks=4, stride=2, bias_term=False):
+	return layers.Deconvolution(bottom,
 			convolution_param=dict(num_output=nout, kernel_size=ks, stride=stride,bias_term = bias_term),
 			param = [dict(lr_mult=0)])
 
