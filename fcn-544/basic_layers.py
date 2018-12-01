5502f73b929813dd2ed52dc4b941accbf762df45
zliu63 <zliu63@illinois.edu>
2018-11-30 22:57:55 -0600

    ..

--- fcn-544/basic_layers.py
+++ fcn-544/basic_layers.py
@@ -4,32 +4,29 @@ from caffe import params
 
 
 def conv(prev, nout, ks=3, stride=1, pad=1):
-	ret = layers.Convolution(prev, kernel_size = ks, stride = stride,
+	return layers.Convolution(prev, kernel_size = ks, stride = stride,
 								num_output=nout, pad = pad,
 								param = [dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)])
-	return ret
+
 
 def relu(prev):
-	ret = layers.ReLu(prev, in_place=True)
-	return ret
+	return layers.ReLu(prev, in_place=True)
 
 def max_pooling(prev, ks=2, stride=2):
-	ret = layers.Pooling(prev, pool=params.Pooling.MAX, kernel_size=ks, stride=stride)
-	return ret
+	return layers.Pooling(prev, pool=params.Pooling.MAX, kernel_size=ks, stride=stride)
+
 
 def dropout(prev, ratio = 0.5, in_place = True):
 	return layers.Dropout(prev, dropout_ratio=ratio, in_place = in_place)
 
 def deconv(prev, nout, ks=4, stride=2, bias_term=False):
-	ret = layers.Deconvolution(prev,
+	return layers.Deconvolution(prev,
 			convolution_param=dict(num_output=nout, kernel_size=ks, stride=stride,bias_term = bias_term),
 			param = [dict(lr_mult=0)])
-	return ret
 
 def sumup(layer1, layer2):
-	ret = layers.Eltwise(layer1, layer2, operation=params.Eltwise.SUM)
-	return ret
+	return layers.Eltwise(layer1, layer2, operation=params.Eltwise.SUM)
+
 def softmax(layer1, layer2):
-	ret = layers.SoftmaxWithLoss(layer1, layer2,
+	return = layers.SoftmaxWithLoss(layer1, layer2,
 				loss_param=dict(normalize=False, ignore_label=255))
-	return ret
