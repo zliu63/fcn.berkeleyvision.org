d3b6c75f1ad308993fff044a64c56c14e2e9eeac
LegendCHN <ningkaiwu@gmail.com>
2018-12-04 15:40:04 -0600

    test2

--- infer.py
+++ infer.py
@@ -14,7 +14,7 @@ in_ -= np.array((104.00698793,116.66876762,122.67891434))
 in_ = in_.transpose((2,0,1))
 
 # load net
-net = caffe.Net('fcn-544/deploy.prototxt', 'fcn-544/snapshot/train_iter_24000.caffemodel', caffe.TEST)
+net = caffe.Net('fcn-544/deploy.prototxt', 'fcn-544/snapshot/train_iter_100000.caffemodel', caffe.TEST)
 # shape for input (data blob is N x C x H x W), set data
 net.blobs['data'].reshape(1, *in_.shape)
 net.blobs['data'].data[...] = in_
@@ -25,6 +25,6 @@ out = net.blobs['score'].data[0].argmax(axis=0)
 # visualize segmentation in PASCAL VOC colors
 voc_palette = vis.make_palette(21)
 out_im = Image.fromarray(vis.color_seg(out, voc_palette))
-out_im.save('demo/test_output.png')
+out_im.save('demo/test2_output.png')
 masked_im = Image.fromarray(vis.vis_seg(im, out, voc_palette))
-masked_im.save('demo/test_visualization.jpg')
+masked_im.save('demo/test2_visualization.jpg')
